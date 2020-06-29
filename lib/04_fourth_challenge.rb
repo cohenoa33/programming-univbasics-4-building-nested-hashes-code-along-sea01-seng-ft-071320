eb33c69447b1d9f654d50c38c820735c9b3d1325
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-06-29 22:00:29 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [ 
        {name: "Benvolio", age: "17", attiude: "worried"}, 
-       {name: , age: , attiude: }, 
+       {name: "Mercutio", age: "18", attiude: }, 
       ]
    }, 
    :capulet => {
